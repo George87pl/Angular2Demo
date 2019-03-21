96ec365f549876224a6932063de1df52a02b31cb
Grzegorz <gpolomicz@gmail.com>
2019-03-21 12:15:23 +0100

    Interpolation + Binding

--- Angular2Demo/src/app/app.component.ts
+++ Angular2Demo/src/app/app.component.ts
@@ -2,6 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'app/app.component.html'
+    template: `<div>
+                    <h1>{{ getFullName() }}</h1>
+                    <img src='https://icdn.2cda.pl/obr/oryginalne/{{imagePath}}' />
+                    <my-employee></my-employee>
+                    <button [disabled]="isDisabled">Click Me</button>
+                </div>`
 })
-export class AppComponent { name = ' Angular!'; }
+export class AppComponent {
+    pageHeader: string = null;
+    imagePath: string = '850ca7d90566b9ed82c60d9d50771230.jpg'
+
+    isDisabled: boolean = false;
+    firstName: string = 'Tom';
+    lastName: string = 'Hopkins';
+
+    getFullName(): string {
+        return this.firstName + ' ' + this.lastName;
+    }
+}
